6dacf88fd857f720e89b807a4cef1a870eb50d05
SsuryaTyagi <2040surya@gmail.com>
2026-06-27 15:47:08 +0530

    deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './assets/Component/Navbar';
 import Home from './assets/Home/Home'
-import "../src/assets/Home/Home.css"
+import "../src/assets/Home/home.css"
 import About from './assets/about/About';
 import Projects from './assets/project/Project';
 import Skills from './assets/skills/Skill';
